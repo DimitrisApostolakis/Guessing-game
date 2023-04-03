0ccec6701e480751a45fc67a6f712193ef07cde1
Dimitris Apostolakis <129792515+DimitrisApostolakis@users.noreply.github.com>
2023-04-03 23:00:46 +0300

    Add files via upload

---
+++ GuessingGame.py
@@ -0,0 +1,26 @@
+#Mini game: Guess the number 1-100
+#Author: Dimitris Apostolakis
+
+import random
+
+secret = random.randint(1, 100)
+print(secret)
+cnt_tries = 1 #Counter for the user's tries.
+cnt_limit = 10 #Maximum attempt limit.
+
+
+print("You have " + str(cnt_limit) + " tries")
+guess = int(input("Give your number: "))
+
+while guess != secret:
+    if guess > secret:
+        print("The secret number is smaller!")
+    elif guess < secret:
+        print("The secret number is larger!")
+    guess = int(input("Give your number: "))
+    cnt_tries += 1
+    if cnt_tries == cnt_limit:
+        print("You have surpass the limit of tries. You lost.")
+        break
+else:
+    print("You found it")
