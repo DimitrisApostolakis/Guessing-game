e2cd35994b62169eff9321b02982eb3b022c23ed
Dimitris Apostolakis <129792515+DimitrisApostolakis@users.noreply.github.com>
2023-04-09 01:28:16 +0300

    Update GuessingGame.py

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,5 +1,9 @@
-#Mini game: Guess the number 1-100
-#Author: Dimitris Apostolakis
+"""
+*****************************************
+Mini game: Guess the number 1-100
+Author: Dimitris Apostolakis
+*****************************************
+"""
 
 import random
 
